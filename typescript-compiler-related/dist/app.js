5428099303ed99836dddb7ffb81c5c20bb5cb005
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-11-29 08:29:20 -0500

    compiler option

---
+++ typescript-compiler-related/dist/app.js
@@ -0,0 +1,9 @@
+"use strict";
+const userName = "Maximilian";
+console.log(userName);
+function add(message, age) {
+    console.log(message, age);
+    return message;
+}
+;
+//# sourceMappingURL=app.js.map
---
+++ typescript-compiler-related/src/app.ts
@@ -0,0 +1,12 @@
+const userName = "Maximilian";
+//userName = 3;
+
+console.log(userName);
+
+
+//code quality options
+//noimplicitResults... nounsudedLocal...noimplicParameters
+function add(message: string, age: number) {
+    console.log(message, age);
+    return message;
+};
