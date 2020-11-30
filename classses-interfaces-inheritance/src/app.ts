1014b6d4b3057d81054f6c1390906e16b3fd5faa
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-11-30 18:42:56 -0500

    intersectionn types on objects and primitve types, discriminates unions in object and using IN in objects and instanceof

--- classses-interfaces-inheritance/src/app.ts
+++ classses-interfaces-inheritance/src/app.ts
@@ -1,52 +1,125 @@
-//Interfaces
-
-//TYPE WAY
-type addFn = (a: number, b: number) => number;
-
-let add: addFn;
-add = (n1:number, n2:number) => {return n1 + n2};
-
-//INTERFACE WAY DEFINE STRUCUTURE OF FUNCTION
-interface addFnInterfaceWay{
-    (a: number, b: number):number; //anonymous function defined structure function type
-}
-let add_2: addFnInterfaceWay;
-add_2 = (n1:number, n2:number) => {return n1 + n2};
-
-
-interface Named {
-    readonly name: string;
-}
-
-interface Person {
+//Intersection Types
+type Admin = {
     name: string;
-    age?: number; //optional paramater
-    greet(phrase: string): void;
+    privileges: string[];
+};
+
+type Employee = {
+    name: string;
+    startDate: Date;
+};
+
+//Interface Way
+//interface ElevatedEmployee extends Employee, Admin {}
+
+type ElevatedEmployee =  Admin & Employee;
+
+const e1: ElevatedEmployee = {
+    name: 'Hector',
+    privileges: ['create-server'],
+    startDate: new Date()
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+//adition of two types and only get intersection...
+type Universal = Combinable & Numeric;
+
+const num1: Universal = 10;
+
+//USED AS TYPED GUARDS... to know type whe are getting at runtime
+function add_(a: Combinable, b: Combinable){
+    if(typeof a === 'string' && typeof b === 'string'){
+        return a.toString() + b.toString();
+    }//checking at runtime the type on runtime...
+
+    return 1;
 }
 
-interface Greetable extends Named {// injecting what is inside Named in Here
-    greet(phrase: string): void;
-}
+type unKnowEmployee = Employee | Admin;
 
-
-//Interfaces in Classes
-class Persons implements Greetable{
-    constructor(public name: string, private readonly age: number){
-
+function printEmployeeInformation(emp: unKnowEmployee){
+    console.log('Name: ' + emp.name);
+    //type Guard come to the rescue usual typeguard does not work in objects
+    if('privileges' in emp){//check if property exists
+        console.log('Privilages: ' + emp.privileges);//this not alw ayas exist on args
     }
-    //interface has no concreet implementations parts
-    greet(phrase: string){
-        console.log(this.age);
-        console.log(phrase);
+
+    if('startDate' in emp){
+        console.log('Start Date: ' + emp.startDate)
     }
 }
 
-let user1: Greetable;
+printEmployeeInformation(e1);//e1 Employee & Admin
 
-user1 = new Persons('Maximum', 30);
+//Type in classses
+class Car{
+    drive(){
+        console.log('Driving...');
+    }
+}
 
-user1.greet('Hector');
- 
+class Truck {
+    drive(){
+        console.log('Driving a truck...');
+    }
+
+    loadCargo(amount: number){
+        console.log('loading cargo ...' + amount);
+    }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 =  new Car();
+
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle){
+
+    vehicle.drive();
+    //check if method exists in methods
+    //method 1
+    //if('loadCargo' in vehicle){
+     //   vehicle.loadCargo(2000);
+    //}
+    //method 2
+    if(vehicle instanceof Truck){//check if instance and execute code only if
+        vehicle.loadCargo(2000);
+    }
+    
+}
 
 
-console.log(user1);
+//Discriminates uinions
+
+interface Bird{
+    type: 'bird';//Literal type asigment Creating descraminating union
+    flyingSpeed: number;
+}
+
+
+interface Horse {
+    type: 'horse';
+    runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+
+
+//when objects that are related but have different properties and methods
+function moveAnimal(animal: Animal){
+    let speed;
+    switch(animal.type){
+        case 'bird':
+            speed = animal.flyingSpeed;
+            break;
+        case 'horse':
+            speed = animal.runningSpeed;
+            break;
+    }
+    console.log('Moving at speed: ' +  speed);
+}
+
+moveAnimal({type:'bird', flyingSpeed: 10 });
