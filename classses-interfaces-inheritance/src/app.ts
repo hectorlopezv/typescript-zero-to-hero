93956ba63cc7bd0c18e79cae05a7c81d850bf7f6
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-12-02 04:59:50 -0500

    more on decorators

--- classses-interfaces-inheritance/src/app.ts
+++ classses-interfaces-inheritance/src/app.ts
@@ -176,4 +176,92 @@ class Printer {
 //Build decorator to AUTOMATIC Bind
 const p = new Printer();
 const button = document.querySelector('button')!;
-button.addEventListener('click', p.showMessage)
+button.addEventListener('click', p.showMessage);
+
+
+
+
+
+
+
+interface ValidatorConfig {
+
+    [property: string]: {//puede que haya o no
+        [validateProp: string]: string[]; //['required, 'positive]
+    }
+};
+const registeredValidators: ValidatorConfig = {};
+
+function Required(target: any, propName: string) {
+    registeredValidators[target.constructor.name] = {
+      ...registeredValidators[target.constructor.name],
+      [propName]: [...registeredValidators[target.constructor.name][propName], 'required']
+    };
+  }
+  
+  function PositiveNumber(target: any, propName: string) {
+    registeredValidators[target.constructor.name] = {
+      ...registeredValidators[target.constructor.name],
+      [propName]: [...registeredValidators[target.constructor.name][propName], 'positive']
+    };
+  }
+function validate(obj: any){//call validate
+    //check if passes checks going through each valiadorts
+    const objValidatorConfig = registeredValidators[obj.constructor.name];
+    if(!objValidatorConfig){
+        return true;
+    }
+    let isValid = true;
+    for (const prop in objValidatorConfig){//looping object
+        for(const validator of objValidatorConfig[prop]){
+            console.log('veces que entro')
+            switch(validator){
+                case 'required':
+                    isValid = isValid &&  !!obj[prop];
+                    break;
+                case 'positive':
+                    isValid = isValid && obj[prop] > 0;
+                    break;
+            }
+        }
+
+    }
+    return isValid;
+}
+
+
+//Decorators as Validators ... example in runtime
+class Course {
+    @Required
+    title: string;
+
+    @PositiveNumber
+    price: number;
+
+    constructor(title: string, price: number){
+        this.title = title;
+        this.price = price;
+    }
+}
+
+
+
+
+const courseForm =  document.querySelector('form')!;
+courseForm.addEventListener('submit', event =>{
+    event.preventDefault();
+    //extract the tittle
+    const titleEl = document.querySelector('#title')! as HTMLInputElement;
+    const priceEl = document.querySelector('#price')! as HTMLInputElement;
+
+    const title = titleEl.value
+    const price = +priceEl.value;
+
+    const createdCourse = new Course(title, price);
+
+    if(!validate(createdCourse)){
+        throw new Error('not paso la validacion');
+    }
+    console.log(createdCourse);
+
+})
